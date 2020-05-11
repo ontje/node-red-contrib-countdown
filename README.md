9bc889617ba38158e75f357970d795aa9f6336fe
Ontje Helmich <hello@ontjehelmich.de>
2020-05-11 23:15:11 +0200

    import

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # node-red-contrib-countdown
-A simple countdown node
+
+A simple countdown node. Starts countdown on any input. Immediatly ends countdown on payload "false" or "0".
+Sends a configurable payload on start and end of countdown on output 1 and remainining time in seconds on output 2 (while running). 
+      
+Countdown time can be set dynamically by using a Message with topic set to "control" and a number as payload (number of seconds to count down).
+
+
+Loosely based on prior work by Neil Cherry: https://github.com/linuxha/node-red-contrib-mytimeout
---
+++ countdown.js
@@ -0,0 +1,131 @@
+module.exports = function(RED) {
+    "use strict";
+
+    const isNumber = require('is-number');
+
+    function countdown(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        node.config = config;
+
+        // Local variables
+        var ticker = null;
+        var ticks = -1;
+        var timeout = parseInt(node.config.timer);
+
+        this.status({ fill: "red", shape: "dot", text: "Stopped: " + timeout });
+
+        function startTimer() {
+            timeout = timeout || parseInt(node.config.timer);
+            ticks = timeout;
+
+            node.status({
+                fill: "green", shape: "dot", text: "Running: " + ticks
+            });
+
+            // Timer Message
+            var msg = {}
+            msg.payload = RED.util.evaluateNodeProperty(node.config.payloadTimerStart, node.config.payloadTimerStartType, node); 
+            if (node.config.topic !== '') {
+                msg.topic = node.config.topic;
+            }
+
+            node.send([msg, null]);
+
+            if (!ticker) {
+                ticker = setInterval(function() { node.emit("TIX"); }, 1000);
+            }
+        }
+
+        function stopTimer() {
+            node.status({
+                fill: "red", shape: "dot", text: "Stopped: " + timeout
+            });
+
+            // Timer Message
+            var msg = {}
+            msg.payload = RED.util.evaluateNodeProperty(node.config.payloadTimerStop, node.config.payloadTimerStopType, node); 
+            if (node.config.topic !== '') {
+                msg.topic = node.config.topic;
+            }
+
+            var remainingTicksMsg = { "payload": 0 };
+            node.send([msg, remainingTicksMsg]);
+
+            endTicker();
+        }
+
+        function endTicker() {
+            if (ticker) {
+                clearInterval(ticker);
+                ticker = null;
+            }
+
+            ticks = -1;
+        }
+
+        node.on("TIX", function() {
+            if (ticks > 1) {
+                ticks--;
+
+                var remainingTicksMsg = { "payload": ticks };
+                node.send([null, remainingTicksMsg]);
+        
+                node.status({
+                    fill: "green", shape: "dot", text: "Running: " + ticks
+                });
+
+            } else if (ticks == 1){
+                stopTimer();
+
+                ticks = 0;
+
+            } else {
+                // Do nothing
+            }
+        });
+
+        node.on("input", function (msg) {
+            if (msg.topic === "control") {
+                if (isNumber(msg.payload) && msg.payload > 1) {
+                    timeout = Math.ceil(msg.payload);
+
+                    if (ticker) {
+                        if (node.config.setTimeToNewWhileRunning) {
+                            ticks = msg.payload;
+                            node.status({
+                                fill: "green", shape: "dot", text: "Running: "+ timeout
+                            });
+                        }
+                    } else {
+                        node.status({
+                            fill: "red", shape: "dot", text: "Stopped: "+ timeout
+                        });
+                    }
+                } else {
+                    // do nothing
+                }
+            } else {
+                if (msg.payload === false || msg.payload === 0) {
+                    stopTimer();
+                } else {
+                    if (ticker) {
+                        if (node.config.resetWhileRunning) {
+                            endTicker();
+                            startTimer();
+                        }
+                    } else {
+                        startTimer();
+                    }
+                }
+            }
+        });
+
+        node.on("close", function() {
+            if (ticker) {
+                clearInterval(ticker);
+            }
+        });
+    }
+    RED.nodes.registerType("countdown", countdown);
+}
